aa7089cff3a26f8c3627ffa686a91623413f9190
juliachernik <116951695+juliachernik@users.noreply.github.com>
2022-12-13 18:18:29 +0100

    Create the basic site

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+//Route for home
+app.get("/", (req, res, next) => {
+    console.log(`we received a request for the HOME page`)
+    //res.send(`message`);
+    res.sendFile(__dirname + "/views/home.html");
+});
+
+//Route for contact page
+app.get("/contact", (req, res, next) => {
+    console.log(`we received a request for the CONTACT page`)
+    res.sendFile(__dirname + "/views/contact.html");
+});
+
+app.listen(3001)
