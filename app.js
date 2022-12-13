ce9b91d33012da198e1115a41c7e9df86b57d79b
Rudolf Teibtner <rudolfteibtner@mailbox.org>
2022-12-13 18:53:50 +0100

    solved the lab

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 //Route for home
 app.get("/", (req, res, next) => {
-    console.log(`we received a request for the HOME page`)
-    //res.send(`message`);
-    res.sendFile(__dirname + "/views/home.html");
+  console.log(`we received a request for the HOME page`);
+  //res.send(`message`);
+  res.sendFile(__dirname + "/views/home.html");
 });
 
 //Route for contact page
 app.get("/contact", (req, res, next) => {
-    console.log(`we received a request for the CONTACT page`)
-    res.sendFile(__dirname + "/views/contact.html");
+  console.log(`we received a request for the CONTACT page`);
+  res.sendFile(__dirname + "/views/contact.html");
 });
 
-app.listen(3001)
+app.listen(3001);
